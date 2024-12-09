644df4595700a68cdd644dcb06bf4bfd0d5c4a58
Abdullahalhasan627 <alhasan.200510@gmail.com>
2024-12-09 14:12:14 +0400

    php

--- Labo/index.php
+++ Labo/index.php
@@ -25,4 +25,23 @@
     echo "<br/><br/>";
     echo 1 + (int) "100 hi";  // 101 (INTEGER)  
     echo 1 + (integer) "hi 100";  // 1  (INTEGER)
+    echo 1 + 1.5;
+    echo "<br/><br/>"; 
+    echo gettype(10 + (int) "hi 100"); //  1  (INTEGER)
+    echo "<br/>";
+    echo 1 + (int) "hi 100";
+    echo "<br/>";
+    echo gettype(1 + 1.5);  //  2.5 (DOUBLE)
+    echo "<br/>";    
+    echo gettype(1 + (int) 1.5);  // 2.5  (INTEGER)
+
     
+    var_dump((bool)"");   //  bool(false)
+    var_dump((bool) "0");   // bool(false)
+    var_dump((bool) "1"); // bool(true)
+    var_dump((bool)"hi"); // bool(true)
+    var_dump((bool) []);  // bool(false)
+    var_dump((bool) [1]); // bool(true)
+    var_dump((bool) [2,3]); // bool(true)
+    var_dump((bool) 0);  // bool(false)
+    var_dump((bool) 10234);  // bool(true)
